80b69773be26ec120da2e38153cd0144a6c07582
tektechnologies <info@tektechnologies.com>
2023-04-18 11:45:46 -0500

    add data

--- src/Main.js
+++ src/Main.js
@@ -1,36 +1,40 @@
 import React from 'react';
 import Pizza from './Pizza.js';
+import data from './data.json';
+
+
+
 
 class Main extends React.Component {
 
 
-
   render(){
+    console.log(data);
+
+
+    let pizzas = [];
+    data.forEach((newPizza, index) => {
+      pizzas.push(<Pizza pie={newPizza.name}  imageURL={newPizza.imageURL}  />)
+    });
+
+
+
+
     return(
       <main>
-        {/* I want pizzas here  */}
-        {/* 1. create pizza component and then add some attributes or properties  */}
-
-        {/* {
-          this.pie = "Detroit"
-          this.props = 
-        } */}
-        <Pizza 
+      
+        {/* <Pizza 
          pie="Detroit" 
          toppings="Cheese" 
          crust="Detroit Style"
         image_url="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" />
-        <Pizza pie="New York This" toppings="Cheese" crust="Thin Style"/>
-        {/* <Pizza pie="Chicago Deep Dish" toppings="Cheese" crust="Pizza Style"/>
-        <Pizza pie="Oven Grinder" toppings="Cheese" crust="Pizza Style"/>
-        <Pizza pie="Brick Oven" toppings="Cheese" crust="Pizza Style"/>
-        <Pizza pie="Calzone" toppings="Cheese" crust="Pizza Style"/> */}
+        <Pizza pie="New York This" toppings="Cheese" crust="Thin Style"/> */}
+         {pizzas}
       </main>
     );
   }
 
 
 }
-
 
 export default Main;
--- src/Pizza.js
+++ src/Pizza.js
@@ -1,15 +1,55 @@
 import React from "react";
 
 class Pizza extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //we can have several properties
+      //count likes
+      likes: 0,
+      //assign default values to make state more readable.
+      //global variable would update only on valie and not render state for the component.
+      pizzaNeeded: false
+    };
+  }
+
+  //helper function
+  handleLikes = () => {
+    console.log("on click here");
+    //method is setState which takes an object
+    this.setState({
+      likes: this.state.likes + 1,
+    });
+  };
+
+  pizzaNeeded = () => {
+    //add a method to set state to be true
+  };
+
+  pizzaGot = () => {
+    //set state to be false.
+  };
+
   render() {
-    // console log some data here
-    console.log("our pizza props", this.props.pie);
     return (
       <>
         <article>
-          <img src={this.props.image_url}/>
-          <h3>Toppings: {this.props.toppings}</h3>
-          <p>CRUST: {this.props.crust}</p>
+          <h3>{this.props.pie}</h3>
+          <p> {this.state.likes}Likes!</p>
+          <p onClick={this.handleLikes}>Click to like this Pizza</p>
+          <img src={this.props.imageURL} alt={this.props.pie} />
+
+        {/* conditional rendering for our button */}
+        {/* message to appear if pizza is awesome */}
+       
+        {/* 
+        Ternary Operator
+        What? True : False 
+        BooleanValue ? console.log('true') : console.log('false');
+        */}
+
+        <div>{this.state.pizzaNeeded ? 'I need this Pizza!' : 'hey IM good'}</div>
+        
         </article>
       </>
     );
